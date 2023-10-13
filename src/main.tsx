5cfe248bc339d9875bc2a60ca257602e9e590b78
rebollogeoffrey <geoffrey.rebollo@free.fr>
2023-10-13 11:04:33 +0200

    tranadapatation microService done, ready to work

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./services/App";
+import "./styles/index.css";
+import "./styles/game.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
